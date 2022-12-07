b193e362ad388a8ccebebd72ffb905b03eb677fb
Ivan Perevernykhata <ivan@changingdigital.com>
2022-12-08 10:04:09 +1300

    -cleanup unnecessary complexity

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,25 +1,19 @@
 import { notFound } from "next/navigation";
-// eslint-disable-next-line no-unused-vars
-function asyncComponent<T, R>(fn: (arg: T) => Promise<R>): (arg: T) => R {
-  // eslint-disable-next-line no-unused-vars
-  return fn as (arg: T) => R;
-}
 
 export interface Props {
   params: {
     slug: string;
   };
-  isPreviewMode?: boolean;
 }
 
-const Article = asyncComponent(async ({ params, isPreviewMode }: Props) => {
+const Article = ({ params }: Props) => {
   const { slug } = params;
 
   if (slug !== "works") {
     return notFound();
   }
   return <div>Articles page with slug</div>;
-});
+};
 
 export const revalidate = 10;
 export const dynamicParams = true;
